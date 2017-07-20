405da52037dcb31aa2505718b1bd0fbb363be71b
KyleSoon <562881821@qq.com>
2017-07-20 17:17:03 +0000

    718

--- main/main.js
+++ main/main.js
@@ -1,3 +1,11 @@
-module.exports = function main() {
+module.exports = function main(a)
   // Write your cade here
-};
+{  
+    var m = parseInt(a)
+    var t= m%100    
+    var y=(m-t)/100 
+    var u= (m-100*y)%10 //3
+    var w= (m-100*y-u)/10
+    var result= y+u+w
+    return result;
+}
